8c8cb4826249f943f838f4dbc5e23d6bd8e01ff3
Vincenzo Bruno <123235736+Vinx989@users.noreply.github.com>
2023-05-12 13:00:11 +0200

    commentato al meglio

--- pulisciNumeri.py
+++ pulisciNumeri.py
@@ -1,11 +1,17 @@
+# i numeri che mi servono per i controlli
 numeri= ['0','1','2','3','4','5','6','7','8','9']
+#flag per il ciclo while
 split_on = True
+#variabile che mi servirà come controllo
 a = 0
-numeri_unici= []
+
+numeri_unici= [] #lista vuota per i numeri unici(contenitore)
+
 while split_on:
     numeri_dig = input('Inserisci una secuenza di numeri\n')
-    lista_numeri= list(numeri_dig)
-    print(lista_numeri)
+
+    lista_numeri= list(numeri_dig) #creo la lista di numeri
+
     #check se ha scritto qualcosa che non va
     for num in lista_numeri:
         if num not in numeri:
@@ -13,9 +19,11 @@ while split_on:
             a = num
             break
         else:
+            #aggiungo in numero alla lista
             if int(num) not in numeri_unici:
                 numeri_unici.append(int(num))
 
+    # questo confronto serve per vedere se un valore è risultato essere un NON numero
     if a == 0:
         print(f'I numeri unici sono .\n{numeri_unici}')
         split_on = False
