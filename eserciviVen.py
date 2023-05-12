e5e032ed9ea264e2126d46178450970daf679553
Vincenzo Bruno <123235736+Vinx989@users.noreply.github.com>
2023-05-12 11:07:52 +0200

    Update eserciviVen.py
    Seconda versione un pò più elegante

--- eserciviVen.py
+++ eserciviVen.py
@@ -4,12 +4,13 @@ parola_dig = input('Inserisci una parola a piacere\n')
 
 # trasformo l'input stringa in una lista
 parola = parola_dig.lower()
-parola_lista = list(parola)
+parola_lista = sorted(parola)
 
 #prendo le vocali uniche
 parola_list_un = set(parola_lista)
 
 #per ogni vocale unica confronto ogni vocale presente nella lista creata dalla stringa
+print('Il risultato è il seguente:')
 for vocale in parola_list_un:
     count = 0
     for vocale_cerca in parola_lista:
@@ -17,5 +18,7 @@ for vocale in parola_list_un:
             count +=1
         #a fine ciclo creo un campo per ogni vocale unica a cui assegno il valore della ripetizione
         conteggio[vocale] = count
+    print(f'La lettera {vocale} è ripetuta {count}')
 
-print(conteggio)
+print(f'Riasumendo abbiamo:\n{conteggio}')
+
